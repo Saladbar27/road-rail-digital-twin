cf6ad813c5e8cb3e92eddef1b22c0e469990f190
Shelby Salazar <shelbycsalazar@gmail.com>
2026-06-24 13:16:27 +0100

    Initial project structure

---
+++ learning/tutorial2.py
@@ -0,0 +1,6 @@
+
+distance = float(input("Enter the distance in kilometers: "))
+speed = float(input("Enter the speed in kilometers per hour: "))
+
+time = distance / speed
+print(f"The time it will take to travel {distance} kilometers at {speed} kilometers per hour is {time} hours.")
