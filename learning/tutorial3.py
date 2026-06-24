6dd96b463db526715e30813f628ecac1af4b0cbf
Shelby Salazar <shelbycsalazar@gmail.com>
2026-06-24 14:45:30 +0100

    Continued Corey Schafer Videos, midway through 4

---
+++ learning/tutorial3.py
@@ -0,0 +1,38 @@
+
+# Arithmetic Operators:
+# Addition:         3 + 2
+# Subtraction:      3 - 2
+# Multiplication:   3 * 2
+# Division:         3 / 2
+# Floor Division:   3 // 2
+# Exponent:         3 ** 2
+# Modulus (Modulo): 3 % 2, gives us the remainder after a division, can be useful for telling if a number is even or odd
+
+# order of operations can be manipulated with parenthesis
+
+# incrementing variables is very useful
+# num += 1 adds 1 to num
+# num*= 10 multiplies num by 10
+
+# abs(negative number) gives absolute value
+
+# round(number, x) gives a rounded number rounded to x digits after the decimal
+
+# Comparison Operators:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+# Returns BOOLEAN Values (True or False)
+
+# casting: turning variables from str to int, or one form to another
+
+num_1 = '100'
+num_2 = '200'
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
---
+++ learning/tutorial4.py
@@ -0,0 +1,11 @@
+
+# To create a list, use square brackets []
+# len(variable) to see how many items in a list
+# "index" for a list starts at 0, access a specific item using square brackets in the print
+
+# using print(variable[-1]) gives the last item in the list. Good trick.
+# print(variable[0:2]) gives 0 and 1 index, but NOT 2
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+print(courses[2:])
