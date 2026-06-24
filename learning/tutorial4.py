82bc5f12a379e3a6337613fd5c4384dfcbd7ec38
Shelby Salazar <shelbycsalazar@gmail.com>
2026-06-24 15:41:34 +0100

    Finished video 4

--- learning/tutorial4.py
+++ learning/tutorial4.py
@@ -6,6 +6,39 @@
 # using print(variable[-1]) gives the last item in the list. Good trick.
 # print(variable[0:2]) gives 0 and 1 index, but NOT 2
 
-courses = ['History', 'Math', 'Physics', 'CompSci']
+# .append(item) adds an item to the end of a list
+# .insert(index, item) adds an item to a specific index in a list
+# .extend(list) adds a list to the end of another list
+# .remove(item) removes an item from a list
+# .pop() removes the last item in a list, can be saved as a variable and printed to see what was removed
 
-print(courses[2:])
+# .reverse() reverses the order of a list
+# .sort() sorts a list in alphabetical order, or numerical order if numbers are in the list
+# reverse argument can be added to both .sort() and .reverse() to reverse the order of the list
+# sorted(list) returns a sorted list, but does not change the original list
+
+# min(list) returns the smallest item in a list
+# max(list) returns the largest item in a list
+
+# .index(item) returns the index of an item in a list
+# ('item' in list) returns True or False if the item is in the list
+# enumerate(list) returns the index and item in a list, can be used in a for loop to print both index and item
+
+# .join(list) joins a list into a string
+# .split(string) splits a string into a list
+
+# A tuple cannot be modified
+# Lists are mutable, tuples are immutable
+# Lists use square brackets []
+# Tuples use parentheses ()
+
+# Sets are values that are unordered and have no duplicates
+# Uses could be to test if a value is a part of a set or to remove duplicate values
+# .intersection(set) returns the common values between two sets
+# .difference(set) returns the values that are in one set but not the other
+
+# Sets
+cs_courses = {'History', 'Math', 'Physics', 'CompSci'}
+art_courses = {'History', 'Math', 'Art', 'Design'}
+
+print(cs_courses.union(art_courses))
